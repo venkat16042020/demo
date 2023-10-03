b58f996dd992ceea16b81397e75b886cd09cc6bb
kamanivenkat <venkat16042020@gmail.com>
2023-10-03 23:54:58 +0530

    Restaurant

--- src/main/java/com/example/demo/entity/restaurant/Order9.java
+++ src/main/java/com/example/demo/entity/restaurant/Order9.java
@@ -2,6 +2,7 @@ package com.example.demo.entity.restaurant;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -19,7 +20,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Setter
 public class Order9 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String orderId;
     private String itemId;
 //    @Column(length=10, nullable=false, unique=false)
@@ -28,6 +28,14 @@ public class Order9 {
     private Boolean isTakeAway;
     private double discount;
     private String couponId;
-    private double cost, centralGst, stateGst, totalGst, totalCost;
+    private double cost;
+
+
+    @Value("${props.centralGst:0.00}")
+    private double centralGst;
+    @Value("${props.stateGst:0.00}")
+    private  double stateGst;
+    private double totalGst;
+    private double totalCost;
 
 }
